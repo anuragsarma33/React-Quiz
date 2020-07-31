803a43a0e317b76d6003bbd74610f8ad03978a82
Anurag Sarma <anuragsarma33@gmail.com>
2020-07-31 15:10:12 +0530

    Consumed Open Trivia API

---
+++ src/API.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { shuffleArray } from "./utils";
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+
+export type QuestionState = Question & { answers: string[] };
+
+export enum Difficulty {
+  EASY = "easy",
+  MEDIUM = "medium",
+  HARD = "hard",
+}
+
+const fetchQuestions = async (amount: number, difficulty: Difficulty) => {
+  const response = await axios.get(
+    `https://opentdb.com/api.php?amount=${amount}&type=multiple&difficulty=${difficulty}`
+  );
+  return response.data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([
+      ...question.incorrect_answers,
+      question.correct_answer,
+    ]),
+  }));
+};
+
+export default fetchQuestions;
